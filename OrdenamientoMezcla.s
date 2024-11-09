80324f704d032f1e2482479d617cc43db64e63c5
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:29:53 -0800

    Create OrdenamientoMezcla.s

---
+++ OrdenamientoMezcla.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       OrdenamientoMezcla.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que realiza el metodo
+*              ordenamiento mezcla
+*             
+*              
+* Compilación:  as -o OrdenamientoMezcla.o OrdenamientoMezcla.s
+*               gcc -o OrdenamientoMezcla OrdenamientoMezcla.o
+* Ejecución:    ./OrdenamientoMezcla
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
