e759466e7b53962e91461764efbf92d694f65877
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 14:34:34 -0800

    Create InvertirCadena.s

---
+++ InvertirCadena.s
@@ -0,0 +1,21 @@
+/*=======================================================
+* Programa:       InvertirCadena.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que invierte una cadena de caracteres
+*              
+* Compilación:  as -o InvertirCadena.o InvertirCadena.s
+*               gcc -o InvertirCadena InvertirCadena.o
+* Ejecución:    ./InvertirCadena
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
+
+=========================================================*/
