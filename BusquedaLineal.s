2ba02144640a11e1768c89a8a96f643f27608434
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:13:55 -0800

    Create BusquedaLineal.s

---
+++ BusquedaLineal.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       BusquedaLineal.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que realiza el metodo
+*             busqueda lineal
+*              
+*              
+* Compilación:  as -o BusquedaLineal.o BusquedaLineal.s
+*               gcc -o BusquedaLineal BusquedaLineal.o
+* Ejecución:    ./BusquedaLineal
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
