e106e2ad9010078d0c230ccec697febc5e720546
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:07:27 -0800

    Create MaximoArreglo.s

---
+++ MaximoArreglo.s
@@ -0,0 +1,26 @@
+/*=======================================================
+* Programa:       MaximoArreglo.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que encuentra el
+*              valor maximo en un arreglo
+*             
+*              
+* Compilación:  as -o MaximoArreglo.o MaximoArreglo.s
+*               gcc -o MaximoArreglo MaximoArreglo.o
+* Ejecución:    ./MaximoArreglo
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
