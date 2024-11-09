9150ff700a09e76e3499d15a87792f49cd64e551
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 14:33:31 -0800

    Create NumeroPrimo.s

---
+++ NumeroPrimo.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       NumeroPrimo.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que verifica si un numero
+*              es primo
+*              
+*              
+* Compilación:  as -o NumeroPrimo.o NumeroPrimo.s
+*               gcc -o NumeroPrimo NumeroPrimo.o
+* Ejecución:    ./NumeroPrimo
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            
+*            
+*
+*            
+*            
+*            
+*            
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
