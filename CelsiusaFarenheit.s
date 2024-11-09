7db4bb34b4e8ce6dbf57fdfbd823739e97dd17ea
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 14:30:09 -0800

    Create CelsiusaFarenheit.s

---
+++ CelsiusaFarenheit.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       CelsiusaFarenheit.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que convierte temperatura
+*              celsius a farenheit
+*              
+*              
+* Compilación:  as -o CelsiusaFarenheit.o CelsiusaFarenheit.s
+*               gcc -o CelsiusaFarenheit CelsiusaFarenheit.o
+* Ejecución:    ./CelsiusaFarenheit
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            
+*            
+*
+*            
+*            
+*            
+*            
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
