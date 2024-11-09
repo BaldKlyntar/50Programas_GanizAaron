dd74d8db0459842b2b45e9a13e261e5615ce3fff
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 14:26:52 -0800

    Create MultiplicadosNumeros.s

---
+++ MultiplicadosNumeros.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       MultiplicadosNumeros.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que multiplica
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o MultiplicadosNumeros.o MultiplicadosNumeros.s
+*               gcc -o MultiplicadosNumeros MultiplicadosNumeros.o
+* Ejecución:    ./MultiplicadosNumeros
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x * y;
+*            Console.WriteLine("La multiplicacion es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
