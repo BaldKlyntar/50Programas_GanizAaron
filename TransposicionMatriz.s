d442cf593ce4384bf2f31435b67571d47dece373
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:39:08 -0800

    Create TransposicionMatriz.s

---
+++ TransposicionMatriz.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       TransposicionMatriz.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que realiza transposicion
+*            de matrices
+*              
+*              
+* Compilación:  as -o TransposicionMatriz.o TransposicionMatriz.s
+*               gcc -o TransposicionMatriz TransposicionMatriz.o
+* Ejecución:    ./TransposicionMatriz
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
