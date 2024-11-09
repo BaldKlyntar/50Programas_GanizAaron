42d583fd4a65af4a798730bc81484e09c7ddd5eb
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 14:36:17 -0800

    Create CadenaPalindromo.s

---
+++ CadenaPalindromo.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       CadenaPalindromo.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que verifica si una cadena
+*              es un palindromo
+*              
+*              
+* Compilación:  as -o CadenaPalindromo.o CadenaPalindromo.s
+*               gcc -o CadenaPalindromo CadenaPalindromo.o
+* Ejecución:    ./CadenaPalindromo
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            
+*            
+*
+*           
+*          
+*            
+*           
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
+
+=========================================================*/
