528000f8d32092fe7cb6a2908c06de6949a9239b
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:28:47 -0800

    Create OrdenamientoSeleccion.s

---
+++ OrdenamientoSeleccion.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       OrdenamientoSeleccion.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que realiza el metodo
+*              ordenamiento por seleccion
+*              
+*              
+* Compilación:  as -o OrdenamientoSeleccion.o OrdenamientoSeleccion.s
+*               gcc -o OrdenamientoSeleccion OrdenamientoSeleccion.o
+* Ejecución:    ./OrdenamientoSeleccion
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
