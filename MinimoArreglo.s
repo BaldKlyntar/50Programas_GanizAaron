a988933d7d9c098f0a216214a8a247a41a7178fd
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:08:31 -0800

    Create MinimoArreglo.s

---
+++ MinimoArreglo.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       MinimoArreglo.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que encuentra el
+*              valor minimo en un arreglo
+*              
+*              
+* Compilación:  as -o MinimoArreglo.o MinimoArreglo.s
+*               gcc -o MinimoArreglo MinimoArreglo.o
+* Ejecución:    ./MinimoArreglo
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
