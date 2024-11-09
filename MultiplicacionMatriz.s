626844c9c2497420b9589747c71e3f5e32891269
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:38:11 -0800

    Create MultiplicacionMatriz.s

---
+++ MultiplicacionMatriz.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       MultiplicacionMatriz.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa realiza multiplicacion
+*            de matrices
+*              
+*              
+* Compilación:  as -o MultiplicacionMatriz.o MultiplicacionMatriz.s
+*               gcc -o MultiplicacionMatriz MultiplicacionMatriz.o
+* Ejecución:    ./MultiplicacionMatriz
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
