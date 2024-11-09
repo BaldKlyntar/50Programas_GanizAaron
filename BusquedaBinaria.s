d52677d76393c502c8b2e3ece1479ded3ffe5322
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:14:57 -0800

    Create BusquedaBinaria.s

---
+++ BusquedaBinaria.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       BusquedaBinaria.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que realiza el metodo
+*              busqueda binaria
+*              
+*              
+* Compilación:  as -o BusquedaBinaria.o BusquedaBinaria.s
+*               gcc -o BusquedaBinaria BusquedaBinaria.o
+* Ejecución:    ./BusquedaBinaria
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
