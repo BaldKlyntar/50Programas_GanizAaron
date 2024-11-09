6128da3a7630d8860d4367455271db7819ebbe97
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:27:47 -0800

    Create OrdenamientoBurbuja.s

---
+++ OrdenamientoBurbuja.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       OrdenamientoBurbuja.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que realiza el metodo
+*              ordenamiento burbuja
+*              
+*              
+* Compilación:  as -o OrdenamientoBurbuja.o OrdenamientoBurbuja.s
+*               gcc -o OrdenamientoBurbuja OrdenamientoBurbuja.o
+* Ejecución:    ./OrdenamientoBurbuja
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
