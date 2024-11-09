02305b0251b3219fc14132e61e9bda8e6517b728
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-09 15:30:51 -0800

    Create SumaMatrices.s

---
+++ SumaMatrices.s
@@ -0,0 +1,25 @@
+/*=======================================================
+* Programa:       SumaMatrices.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa para la suma de
+*              matrices
+*             
+*              
+* Compilación:  as -o SumaMatrices.o SumaMatrices.s
+*               gcc -o SumaMatrices SumaMatrices.o
+* Ejecución:    ./SumaMatrices
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
+
+=========================================================*/
+
+	
