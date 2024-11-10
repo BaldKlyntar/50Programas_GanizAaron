8a74bd27f93dfcb65b396acc1d8fdab667576f3c
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 13:18:27 -0800

    Update MCD.s

--- MCD.s
+++ MCD.s
@@ -1,1 +1,41 @@
+/*=======================================================
+* Programa:       MCD.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que calcula el maximo
+*              comun divisor
+*
+* Compilación:  as -o MCD.o MCD.s
+*               gcc -o MCD MCD.o
+* Ejecución:    ./MCD
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+* static void Main()
+*    {
+*        int a, b;
+*        a = 56;
+*        b = 98;
+*
+*        while (a != b)
+*        {
+*            if (a > b)
+*                a = a - b;
+*            else
+*                b = b - a;
+*        }
+*
+*       Console.WriteLine("El MCD es: " + a);
+*    }
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
+
+=========================================================*/
+
+
+
 
