328d315eab8d80bf4ef9a61242c8cfa289c6c730
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 13:22:53 -0800

    Update SumaMatrices.s

--- SumaMatrices.s
+++ SumaMatrices.s
@@ -13,7 +13,39 @@
 *
 * Código equivalente en C#:
 * -----------------------------------------------------
+*	static void Main()
+*    {
+*        int[,] matriz1 = {
+*            {1, 2, 3},
+*            {4, 5, 6},
+*            {7, 8, 9}
+*        };
 *
+*        int[,] matriz2 = {
+*            {9, 8, 7},
+*            {6, 5, 4},
+*            {3, 2, 1}
+*        };
+*
+*        int[,] resultado = new int[3, 3];
+*
+*        for (int i = 0; i < 3; i++)
+*        {
+*            for (int j = 0; j < 3; j++)
+*            {
+*                resultado[i, j] = matriz1[i, j] + matriz2[i, j];
+*            }
+*        }
+*
+*        for (int i = 0; i < 3; i++)
+*       {
+*            for (int j = 0; j < 3; j++)
+*            {
+*                Console.Write(resultado[i, j] + " ");
+*           }
+*            Console.WriteLine();
+*        }
+*    }
 *
 * ASCIINEMA
 * -----------------------------------------------------
