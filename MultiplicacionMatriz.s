b2adbd79f82502f5dc7d07ecdb879ccdf01b0926
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 13:24:24 -0800

    Update MultiplicacionMatriz.s

--- MultiplicacionMatriz.s
+++ MultiplicacionMatriz.s
@@ -13,7 +13,42 @@
 *
 * Código equivalente en C#:
 * -----------------------------------------------------
+*    static void Main()
+*   {
+*        int[,] matriz1 = {
+*            {1, 2, 3},
+*            {4, 5, 6}
+*        };
 *
+*        int[,] matriz2 = {
+*            {7, 8},
+*            {9, 10},
+*            {11, 12}
+*        };
+*
+*        int[,] resultado = new int[2, 2];
+*
+*        for (int i = 0; i < 2; i++)
+*        {
+*            for (int j = 0; j < 2; j++)
+*            {
+*                resultado[i, j] = 0;
+*                for (int k = 0; k < 3; k++)
+*                {
+*                    resultado[i, j] += matriz1[i, k] * matriz2[k, j];
+*                }
+*            }
+*        }
+*
+*        for (int i = 0; i < 2; i++)
+*        {
+*            for (int j = 0; j < 2; j++)
+*            {
+*                Console.Write(resultado[i, j] + " ");
+*            }
+*            Console.WriteLine();
+*        }
+*    }
 *
 * ASCIINEMA
 * -----------------------------------------------------
