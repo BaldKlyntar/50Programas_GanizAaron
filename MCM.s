769408422e0b09d136d65ade2ac1ed4c6efc28c8
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 13:20:23 -0800

    Update MCM.s

--- MCM.s
+++ MCM.s
@@ -1,1 +1,43 @@
+/*=======================================================
+* Programa:       MCM.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que calcula el minimo
+*              comun multiplo
+*
+* Compilación:  as -o MCM.o MCM.s
+*               gcc -o MCM MCM.o
+* Ejecución:    ./MCM
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*      static void Main()
+*    {
+*        int a, b;
+*        a = 56;
+*        b = 98;
+*
+*        int mcd = a, tempA = a, tempB = b;
+*
+*        while (tempA != tempB)
+*        {
+*            if (tempA > tempB)
+*                tempA = tempA - tempB;
+*            else
+*                tempB = tempB - tempA;
+*        }
+*
+*        mcd = tempA;
+*
+*        int mcm = (a * b) / mcd;
+*
+*        Console.WriteLine("El MCM es: " + mcm);
+*    }
+*
+* ASCIINEMA
+* -----------------------------------------------------
+*
+* -----------------------------------------------------
 
+=========================================================*/
