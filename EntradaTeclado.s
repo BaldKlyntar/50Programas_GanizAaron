5d1f338602c0eeb552fc32bd428d593c54dbbee9
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 11:49:53 -0800

    Update EntradaTeclado.s

--- EntradaTeclado.s
+++ EntradaTeclado.s
@@ -25,33 +25,38 @@
 
 =========================================================*/
 
-.section .data
 
-  msg_output: .space 100
-  msg_input: .asciz "Ingresa una cadena de caracteres: \n"
-  format: .asciz "%s"
+.section .data
+    format: .asciz "%s"
+    palabra: .space 100             // Espacio para 100 caracteres
+    msg_prueba: .asciz "La palabra ingresada es: %s\n"
+    msg_ingreso: .asciz "Ingrese una palabra: "
 
 .section .text
 
-  .global main
-  .extern printf
-  .extern scanf
+    .global main
+    .extern printf
+    .extern scanf
 
 main:
+    // Solicitar la entrada del usuario
+    ldr x0, =msg_ingreso
+    bl printf
 
-  ldr x0, =msg_input
-  bl printf
+    // Leer la cadena y almacenarla en 'palabra'
+    ldr x0, =format
+    ldr x1, =palabra
+    bl scanf
 
-  adr x0, =format
-  adr x1, msg_output
-  bl scanf
+    // Imprimir la cadena ingresada
+    ldr x0, =msg_prueba
+    ldr x1, =palabra
+    bl printf
 
-  ldr x0, =msg_output
-  bl scanf
-
-      // Salir del programa
+    // Salir del programa
 
     mov     x0, #0 // Código de estado 0 (indica éxito)
     mov     x8, #93 // Número de syscall para 'exit' (93 en ARM64)
     svc     0 // Se realiza la llamda al sistema
+
 
