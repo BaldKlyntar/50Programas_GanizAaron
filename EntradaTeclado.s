a2cbd313f574367f59cd5a32da24df5b0054c6fb
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-10 11:37:19 -0800

    Update EntradaTeclado.s

--- EntradaTeclado.s
+++ EntradaTeclado.s
@@ -1,1 +1,50 @@
+/*=======================================================
+* Programa:       EntradaTeclado.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        09 de Noviembre de 2024
+* Descripción: Programa que lee
+*              una entrada desde el teclado
+*             
+*              
+* Compilación:  as -o EntradaTeclado.o EntradaTeclado.s
+*               gcc -o EntradaTeclado EntradaTeclado.o
+* Ejecución:    ./EntradaTeclado
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese una cadena de caracteres");
+*            string palabra = Console.ReadLine();
+*            Console.WriteLine(palabra)
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* 
+* -----------------------------------------------------
 
+=========================================================*/
+
+.section .data
+
+  msg_output: .space 100
+  msg_input: .asciz "Ingresa una cadena de caracteres: \n"
+  format: .asciz "%s"
+
+.section .text
+
+  .global main
+  .extern printf
+  .extern scanf
+
+main:
+
+  ldr x0, =msg_input
+  bl printf
+
+  adr x0, =format
+  adr x1, msg_output
+  bl scanf
+
+  ldr x0, =msg_output
+  bl scanf
