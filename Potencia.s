5838dd9b5de8bac5b3b1cab4307e61d10e0e4c90
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:21:13 -0800

    Create Potencia.s

---
+++ Potencia.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       Potencia.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o Potencia.o Potencia.s
+*               gcc -o Potencia Potencia.o
+* Ejecución:    ./Potencia
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
