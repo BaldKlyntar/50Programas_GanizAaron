9603e53c37b5d505064eb3deffafe3a4d4e56bad
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:23:51 -0800

    Create ArregloCola.s

---
+++ ArregloCola.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       ArregloCola.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o ArregloCola.o ArregloCola.s
+*               gcc -o ArregloCola ArregloCola.o
+* Ejecución:    ./ArregloCola
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
