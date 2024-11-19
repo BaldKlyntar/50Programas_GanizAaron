2d4180818c681e001dcab3fb9447403b31259981
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-19 00:10:31 -0800

    Update OperacionesLogicas.s

--- OperacionesLogicas.s
+++ OperacionesLogicas.s
@@ -13,18 +13,70 @@
 *
 * Código equivalente en C#:
 * -----------------------------------------------------
-*            Console.WriteLine("Ingrese el primer valor");
-*            float x = float.Parse(Console.ReadLine());
+*    static void Main()
+*    {
+*        int a = 5;  
+*        int b = 3;  
 *
-*            Console.WriteLine("Ingrese el segundo valor");
-*            float y = float.Parse(Console.ReadLine());
-*            float resta = x - y;
-*            Console.WriteLine("La suma es: {0}", resta);
+*        int andResult = a & b;  
+*        int orResult = a | b;   
+*        int xorResult = a ^ b; 
+*
+*       Console.WriteLine($"AND: {andResult}");
+*        Console.WriteLine($"OR: {orResult}");
+*        Console.WriteLine($"XOR: {xorResult}");
+*    }
 *
 *
 * ASCIINEMA
 * -----------------------------------------------------
-* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* https://asciinema.org/a/QrhCl2j1Oc4qVQiZ35bqvBa8K
 * -----------------------------------------------------
 
 =========================================================*/
+
+    .section .data
+    msg_and:    .asciz "AND Result: %d\n"
+    msg_or:     .asciz "OR Result: %d\n"
+    msg_xor:    .asciz "XOR Result: %d\n"
+    a: .word 5
+    b: .word 3
+
+    .section .text
+    .global main
+    .extern printf
+
+main:
+    // Inicializar los valores
+    ldr x0, =a              // Cargar la dirección de 'a'
+    ldr x29, [x0]            // Cargar el valor de 'a' en x1 (registro de 64 bits)
+
+    ldr x0, =b              // Cargar la dirección de 'b'
+    ldr x28, [x0]            // Cargar el valor de 'b' en x2 (registro de 64 bits)
+
+    // Operación AND
+    and x3, x29, x28          // x3 = a & b (operación AND entre los registros de 64 bits)
+    ldr x0, =msg_and
+    mov x1, x3              // Mover el resultado de AND a x1 para printf
+    bl printf               // Imprimir el resultado
+
+        // Operación OR
+    orr x4, x29, x28          // x3 = a | b (operación OR entre los registros de 64 bits)
+    ldr x0, =msg_or
+    mov x1, x4              // Mover el resultado de OR a x1 para printf
+    bl printf               // Imprimir el resultado
+
+    // Operación XOR
+    eor x5, x29, x28          // x3 = a ^ b (operación XOR entre los registros de 64 bits)
+    ldr x0, =msg_xor
+    mov x1, x5              // Mover el resultado de XOR a x1 para printf
+    bl printf               // Imprimir el resultado
+
+    // Salir del programa
+    mov x0, #0
+    mov x8, #93
+    svc 0
+
+    
+
+
