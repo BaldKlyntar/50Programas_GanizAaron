a2adc03a62463af3b38d7c43eb04690f38701af5
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:06:02 -0800

    Create LongitudCadena.s

---
+++ LongitudCadena.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       LongitudCadena.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o LongitudCadena.o LongitudCadena.s
+*               gcc -o LongitudCadena LongitudCadena.o
+* Ejecución:    ./LongitudCadena
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
