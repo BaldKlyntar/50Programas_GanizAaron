d732a3d1f3b9a8e47a9c276e840e2cbf32850c44
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:09:12 -0800

    Create ManiplucacionBits.s

---
+++ ManiplucacionBits.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       ManipulacionBits.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o ManipulacionBits.o ManipulacionBits.s
+*               gcc -o ManipulacionBits ManipulacionBits.o
+* Ejecución:    ./ManipulacionBits
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
