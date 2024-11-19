19cf193249c332c325f96d381141dd0302afea23
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:27:11 -0800

    Create DecimalHexadecimal.s

---
+++ DecimalHexadecimal.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       DecimalHexadecimal.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o DecimalHexadecimal.o DecimalHexadecimal.s
+*               gcc -o DecimalHexadecimal DecimalHexadecimal.o
+* Ejecución:    ./DecimalHexadecimal
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
