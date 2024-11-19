fbf21419da6dd46e307af14f006bc096a7635711
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-19 02:37:51 -0800

    Create Fibonacci.s

---
+++ Fibonacci.s
@@ -0,0 +1,38 @@
+/*=======================================================
+* Programa:       Fibonacci.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o Fibonacci.o Fibonacci.s
+*               gcc -o Fibonacci Fibonacci.o
+* Ejecución:    ./Fibonacci
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*      static void Main()
+*    {
+*        string input = "Ejemplo de texto";
+*        int vowels = 0, consonants = 0;
+*        
+*        foreach (char c in input)
+*        {
+*            if ("aeiouAEIOU".IndexOf(c) >= 0)
+*                vowels++;
+*            else if (char.IsLetter(c))
+*                consonants++;
+*        }
+*
+*        Console.WriteLine($"Vocales: {vowels}, Consonantes: {consonants}");
+*    }
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/3gWego73jhm7YsZJZRRjcLhFz
+* -----------------------------------------------------
+
+=========================================================*/
