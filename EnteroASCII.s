892f7426a48a223040e6065567b5033bb252b4ef
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:05:26 -0800

    Create EnteroASCII.s

---
+++ EnteroASCII.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       EnteroASCII.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que resta
+*              dos numeros ingresados por
+*              el usuario.
+*              
+* Compilación:  as -o EnteroASCII.o EnteroASCII.s
+*               gcc -o EnteroASCII EnteroASCII.o
+* Ejecución:    ./EnteroASCII
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
