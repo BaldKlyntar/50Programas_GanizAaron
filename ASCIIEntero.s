f57e0358e78365cdab6680f048a73d0f77b07b3d
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2024-11-18 16:04:50 -0800

    Create ASCIIEntero.s

---
+++ ASCIIEntero.s
@@ -0,0 +1,30 @@
+/*=======================================================
+* Programa:       ASCIIEntero.s
+* Autor:        Ganiz Galaz Aaron
+* No.Control:   23211003
+* Fecha:        06 de Noviembre de 2024
+* Descripción: Programa que convierte un caracter
+*              ASCII a entero
+*              
+*              
+* Compilación:  as -o ASCIIEntero.o ASCIIEntero.s
+*               gcc -o ASCIIEntero ASCIIEntero.o
+* Ejecución:    ./ASCIIEntero
+*
+* Código equivalente en C#:
+* -----------------------------------------------------
+*            Console.WriteLine("Ingrese el primer valor");
+*            float x = float.Parse(Console.ReadLine());
+*
+*            Console.WriteLine("Ingrese el segundo valor");
+*            float y = float.Parse(Console.ReadLine());
+*            float resta = x - y;
+*            Console.WriteLine("La suma es: {0}", resta);
+*
+*
+* ASCIINEMA
+* -----------------------------------------------------
+* https://asciinema.org/a/JqirVW0Vy6vL8KDMk80bPEwev
+* -----------------------------------------------------
+
+=========================================================*/
